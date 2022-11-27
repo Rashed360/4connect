31f87f5716a0fb556dbd97027bacc0b90a4d0e29
Rashed Ahmed <rashed.ahmed@email.com>
2022-11-27 21:21:51 +0600

    state indicator

--- components/GameBoard.js
+++ components/GameBoard.js
@@ -1,12 +1,15 @@
 const GameBoard = () => {
 	return (
-		<div className='game-board'>
-			{[...Array(42)].map((itm, idx) => (
-				<div className='slot' key={idx}>
-					<div className='highlight'></div>
-				</div>
-			))}
-		</div>
+		<>
+			<div className='game-board'>
+				{[...Array(42)].map((itm, idx) => (
+					<div className='slot' key={idx}>
+						<div className='highlight'></div>
+					</div>
+				))}
+			</div>
+			<div className='game-state'></div>
+		</>
 	)
 }
 
